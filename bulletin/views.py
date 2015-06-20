68332f46d4341d390a09ce722712a9c710833de3
Miles Wen <miles.h.f.wen@gmail.com>
2015-06-20 20:26:22 +0800

    Merge remote-tracking branch 'origin/jieming' into Miles-branch
    Conflicts:
    	RoomieHK/settings.pyc
    	bulletin/urls.py

--- bulletin/views.py
+++ bulletin/views.py
@@ -22,7 +22,32 @@ def preferences(request, user_id):
 
 # return bulletin listing
 def listing(request, user_id):
-	return render(request, 'bulletin/listing.html')
+	# user_id is the id of a room SEEKer
+	# scenario: given the preferences of a room seeker user_id, return a list of rooms based on the preferences
+	context_dict = {}
+	
+	try:
+		seeker = User.objects.get(id=user_id)
+		#preference attributes (to owners): gender_mate, occupation_mate, hasPet_mate, smoker_mate, quiet_mate,
+		#preference attributes (to seekers): same as above
+		#preference attributes (to rooms): district, isPrivate, price, moveInDate, stayPeriod, 
+		
+		#currently only consider 4 preference filters
+		# 1. the room owner's gender should match the seeker's gender_mate preference
+		# 2. the district, isPrivate, price attributes of the room should match the seeker's district, isPrivate, price attributes
+		
+# 		print seeker.gender_mate
+# 		print seeker.district, seeker.isPrivate, seeker.price 
+		
+		rooms = Room.objects.filter(user__gender=seeker.gender_mate, district=seeker.district, isPrivate=seeker.isPrivate, price__lte = seeker.price)
+		context_dict['roomList'] = rooms
+		context_dict['seeker'] = seeker
+		
+	
+	except User.DoesNotExist, Room.DoesNotExist:
+		pass
+	
+	return render(request, 'bulletin/listing.html', context_dict)
 
 # return bulletin details
 def details(request, room_id):
@@ -33,9 +58,6 @@ def details(request, room_id):
 		room = Room.objects.get(id=room_id)
 		context_dict['roomInfo'] = room
 		context_dict['ownerInfo'] = room.user
-		
-		owner_id = room.id
-		print room.id
 	
 	except Room.DoesNotExist:
 		pass
