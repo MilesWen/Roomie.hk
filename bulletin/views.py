630d393afcabf85090f2782064f0e582d3f7b938
jmshi123 <jieming@GuessWho.local>
2015-06-21 06:40:46 +0800

    views session

--- bulletin/views.py
+++ bulletin/views.py
@@ -5,6 +5,7 @@ from bulletin.models import User, Room, Bookmark, Friend
 
 from django.contrib.auth import authenticate, login
 from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.decorators import login_required
 
 
 # Get FB img
@@ -33,6 +34,7 @@ def login_FB(request):
 	return render(request, 'bulletin/login.html')
 
 # information from facebook
+@login_required(login_url='/')
 def social_login(request):
 	social_user = request.user.social_auth.filter(provider='facebook',).first()
 	if social_user:	
@@ -67,11 +69,12 @@ def social_login(request):
 			userFriend = Friend(user=user, friend=str(frdLst))
 			userFriend.save()
 			
-		return preference(request)
+		return preferences(request)
 	else:
 		return None
 
 # show profile of user
+@login_required(login_url='/')
 def profile(request):
 	# scenario: user wants to check the profile of the roomate/room owner
 	context_dict = {}
@@ -87,6 +90,7 @@ def profile(request):
 	return render(request, 'bulletin/profile.html',context_dict)
 
 # ask for preference (filtering info) from user
+@login_required(login_url='/')
 def preferences(request):
 	context_dict = {}
 	# scenario: user wants to check his/her preferences
@@ -102,14 +106,16 @@ def preferences(request):
 	return render(request, 'bulletin/preferences.html',context_dict)
 
 # return bulletin listing
+@login_required(login_url='/')
 def listing(request):
 	# user_id is the id of a room SEEKer
 	# scenario: given the preferences of a room seeker user_id, return a list of rooms based on the preferences
 	context_dict = {}
 	
 	try:
+		print request.session['userid']
 		seeker = User.objects.get(id=request.session['userid'])
-
+		
 		#preference attributes (to owners): gender_mate, occupation_mate, hasPet_mate, smoker_mate, quiet_mate,
 		#preference attributes (to seekers): same as above
 		#preference attributes (to rooms): district, isPrivate, price, moveInDate, stayPeriod, 
@@ -131,6 +137,7 @@ def listing(request):
 	return render(request, 'bulletin/listing.html', context_dict)
 
 # return bulletin details
+@login_required(login_url='/')
 def details(request, room_id):
 	# scenario: user views the details of a room and its owner details
 	context_dict = {}
